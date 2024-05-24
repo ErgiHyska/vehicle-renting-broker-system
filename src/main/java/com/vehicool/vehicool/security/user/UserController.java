9c980df9ffd3c578c1a1a5fcea1deb243ac46b48
ErgiHyska <ehyska21@epoka.edu.al>
2024-05-24 19:13:34 +0200

    Updated

--- src/main/java/com/vehicool/vehicool/security/user/UserController.java
+++ src/main/java/com/vehicool/vehicool/security/user/UserController.java
@@ -3,6 +3,7 @@ package com.vehicool.vehicool.security.user;
 import com.vehicool.vehicool.business.service.DataPoolService;
 import com.vehicool.vehicool.business.service.LenderService;
 import com.vehicool.vehicool.business.service.RenterService;
+import com.vehicool.vehicool.persistence.entity.Lender;
 import com.vehicool.vehicool.persistence.entity.Renter;
 import com.vehicool.vehicool.util.mappers.ResponseMapper;
 import lombok.RequiredArgsConstructor;
@@ -41,11 +42,12 @@ public class UserController {
             return ResponseMapper.map(FAIL, HttpStatus.INTERNAL_SERVER_ERROR, null, ERROR_OCCURRED);
         }
     }
+
     @PostMapping("/user-ban-appeal")
     public ResponseEntity<?> banAppeal(Principal connectedUser) {
         try {
             User user = userRepository.findByUsername(connectedUser.getName()).orElse(null);
-            if(user==null){
+            if (user == null) {
                 return ResponseMapper.map(FAIL, HttpStatus.BAD_REQUEST, null, "USER NOT FOUND!");
             }
             return ResponseMapper.map(SUCCESS, HttpStatus.OK, user, RECORDS_RECEIVED);
@@ -53,14 +55,15 @@ public class UserController {
             return ResponseMapper.map(FAIL, HttpStatus.INTERNAL_SERVER_ERROR, null, ERROR_OCCURRED);
         }
     }
+
     @PostMapping("/user-renter-profile-application")
     public ResponseEntity<?> renterProfileApplication(Principal connectedUser) {
         try {
             User user = userRepository.findByUsername(connectedUser.getName()).orElse(null);
-            if(user==null){
+            if (user == null) {
                 return ResponseMapper.map(FAIL, HttpStatus.BAD_REQUEST, null, "USER NOT FOUND!");
             }
-            if(user.getRenterProfile().equals(null)){
+            if (user.getRenterProfile().equals(null)) {
                 Renter renter = new Renter();
                 renter.setUser(user);
                 renter.setStatus(dataPoolService.findByEnumLabel("unconfirmedRenter"));
@@ -70,6 +73,28 @@ public class UserController {
                 return ResponseMapper.map(SUCCESS, HttpStatus.OK, renter, RECORDS_RECEIVED);
             }
             return ResponseMapper.map(FAIL, HttpStatus.BAD_REQUEST, null, "User have already a renter profile!");
+        } catch (Exception e) {
+            return ResponseMapper.map(FAIL, HttpStatus.INTERNAL_SERVER_ERROR, null, ERROR_OCCURRED);
+        }
+    }
+
+    @PostMapping("/user-lender-profile-application")
+    public ResponseEntity<?> lenderProfileApplication(Principal connectedUser) {
+        try {
+            User user = userRepository.findByUsername(connectedUser.getName()).orElse(null);
+            if (user == null) {
+                return ResponseMapper.map(FAIL, HttpStatus.BAD_REQUEST, null, "USER NOT FOUND!");
+            }
+            if (user.getLenderProfile().equals(null)) {
+                Lender lender = new Lender();
+                lender.setUser(user);
+                lender.setStatus(dataPoolService.findByEnumLabel("unconfirmedLender"));
+                lenderService.save(lender);
+                user.setLenderProfile(lender);
+                userRepository.save(user);
+                return ResponseMapper.map(SUCCESS, HttpStatus.OK, lender, RECORDS_RECEIVED);
+            }
+            return ResponseMapper.map(FAIL, HttpStatus.BAD_REQUEST, null, "User have already a lender profile!");
         } catch (Exception e) {
             return ResponseMapper.map(FAIL, HttpStatus.INTERNAL_SERVER_ERROR, null, ERROR_OCCURRED);
         }
